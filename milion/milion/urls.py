cec1f92f2fe554ad5b1dda11233b372f5e8b0f8c
passiondev11 <orlovwork11@gmail.com>
2020-04-20 11:11:39 +1000

    Add user login/signup page

--- milion/milion/urls.py
+++ milion/milion/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('', include('home.urls')),
-    path('home/', include('home.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
+    path('user/', include('user.urls')),
+    path('user/', include('django.contrib.auth.urls')),
 ]
